0652c5596111488d29750a3fcc5bc4556862b5b1
Akua-95 <serwaa.precious@gmail.com>
2024-06-30 12:17:58 +0000

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python3
-""" 3-app module """
-from typing import Union
-from flask import Flask, request
-from flask_babel import Babel
-from routes.routes_3 import app_routes
-from config import Config
-
+"""
+3. Parametrize templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 babel = Babel(app)
 
+
+class Config(object):
+    """
+    Config class for Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
-app.register_blueprint(app_routes)
+
+
+@app.route('/')
+def index():
+    """
+    Renders index.html template
+    """
+    return render_template(
+        '3-index.html', title=gettext("Welcome to Holberton"),
+        header=gettext("Hello world")
+        )
 
 
 @babel.localeselector
-def get_locale() -> Union[str, None]:
-    """ get locale
+def get_locale():
     """
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    Determines the best match for the supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
